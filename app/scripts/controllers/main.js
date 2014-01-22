42c6b802030ba3f28ae5bc1e91aaa3570d290935
bouzuya <bouzuya@gmail.com>
2014-01-22 22:14:55 +0900

    yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('hatenaGraphMobileApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
